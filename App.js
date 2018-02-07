b80e978cfd620eae53fbd2bf4caa15243f5a0590
Alejandro E. Rendon <alejorendon@utp.edu.co>
2018-02-07 10:13:58 -0500

    First screen

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Alert, StyleSheet, Text, View, Button, Image } from 'react-native';
 
+import { Ionicons } from '@expo/vector-icons';
+
 export default class App extends React.Component {
 
   render() {
-    const image = 'http://images.coveralia.com/audio/d/David_Bowie-Best_Of_Bowie_(Special_Edition)-Frontal.jpg'
-    const name = 'David Bowie'
+    const image = 'http://images.coveralia.com/audio/d/David_Bowie-Best_Of_Bowie_(Special_Edition)-Frontal.jpg';
+    const name = 'David Bowie';
+    const likes = 200;
+    const comments = 140;
 
     return (
       <View style={styles.container}>
@@ -13,6 +17,16 @@ export default class App extends React.Component {
           <Image style={styles.image} source={{ uri: image }} />
           <View style={styles.info}>
             <Text style={styles.name}>{name}</Text>
+            <View style={styles.row}>
+              <View style={styles.iconContainer}>
+                <Ionicons name='ios-heart-outline' size={30} color='gray' />
+                <Text style={styles.count}>{likes}</Text>
+              </View>
+              <View style={styles.iconContainer}>
+                <Ionicons name='ios-chatboxes-outline' size={30} color='gray' />
+                <Text style={styles.count}>{comments}</Text>
+              </View>
+            </View>
           </View>
         </View>
       </View>
@@ -36,11 +50,25 @@ const styles = StyleSheet.create({
   },
   info: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
+    alignItems: 'center',
     justifyContent: 'center',
   },
   name: {
     fontSize: 20,
     marginTop: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 40,
+    marginTop: 15,
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  count: {
+    color: 'gray',
   }
 });
